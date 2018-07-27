e6b63de4162ed6899cd31f718b8509b95d741663
Harley Rowland <harleyrowland17@gmail.com>
2018-07-27 07:09:20 +0100

    Styling changes for product page

--- services/productService.js
+++ services/productService.js
@@ -6,10 +6,13 @@ var moneyTransformer = require('../utils/moneyTransformer.js');
 module.exports = {
 	getProduct: function(req, res, template, callback) {
 		var data = {}
-		console.log(req.query)
 		data.product = JSON.parse(fileAccess.getSingleFile("public/products/" + req.query.productId + ".json"));
 		data.product.displayPrice = moneyTransformer.getDisplayPrice(data.product.price);
-	  	
+		data.product.similarProducts = data.product.similarProducts.map(function(similarProduct){ 
+			var product = JSON.parse(fileAccess.getSingleFile("public/products/" + similarProduct + ".json"));
+			product.displayPrice = moneyTransformer.getDisplayPrice(product.price);
+			return product;
+		});
 	  	return callback(null, res, template, data);
 	}
 }
